d4a1e6bf5f52cb49a5ae6f038c1bcc37447ac54a
jisanahamed09205 <jisanahamed09205@gmail.com>
2024-02-19 01:42:58 +0600

    banner section done

--- portfolio-2.0-frontend/src/App.jsx
+++ portfolio-2.0-frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import Hero from "./Component/Hero/Hero";
 import Navbar from "./Component/Navbar/Navbar";
+import './App.css'
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <div className="container">
+        <Hero/>
+      </div>
+    </>
   );
 };
 
---
+++ portfolio-2.0-frontend/src/Component/Hero/Hero.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-unescaped-entities */
+import './Hero.css'
+import react from '../../assets/images/react.png'
+import tailwind from '../../assets/images/tailwind.png'
+import javascript from '../../assets/images/javascript.png'
+import nodejs from '../../assets/images/nodejs.png'
+import pp from '../../assets/images/pp.jpg'
+
+const Hero = () => {
+    return (
+        <section className='hero-container'>
+            <div className='hero-content'>
+                <h2>Hello there! 👋 I'm<br /> Jisan Molla,</h2>
+                <h4>a <span style={{color: 'aquamarine'}}>Web Developer</span> </h4>
+                <p>on a mission to craft digital experiences that captivate and inspire. My journey into the world of web development began with a curiosity for technology and a passion for problem-solving. Armed with a toolkit that includes MongoDB, Express.js, React.js, and Node.js, I'm ready to tackle any challenge that comes my way. I'm not just a developer; I'm an artist, weaving together code and creativity to build immersive web applications that leave a lasting impression. Let's collaborate and create something extraordinary together!
+
+</p>
+            </div>
+            <div className='hero-img'>
+                <div>
+                    <div className='tech-icon'>
+                        <img src={react} alt="" />
+                    </div>
+                    <img src={pp} alt="" />
+                </div>
+                <div>
+                    <div className='tech-icon'>
+                        <img src={tailwind} alt="" />
+                    </div>
+                    <div className='tech-icon'>
+                        <img src={javascript} alt="" />
+                    </div>
+                    <div className='tech-icon'>
+                        <img src={nodejs} alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
