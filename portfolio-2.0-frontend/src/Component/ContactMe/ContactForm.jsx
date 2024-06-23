625911bf2a23c0db6321566f24914f7e907a3c78
jisanahamed09205 <jisanahamed09205@gmail.com>
2024-06-23 18:09:05 +0600

    contact form is now active when anyone provide data and then submit the button then it will go on my email i am use ther emailjs who send me to my email inbox

--- portfolio-2.0-frontend/src/Component/ContactMe/ContactForm.jsx
+++ portfolio-2.0-frontend/src/Component/ContactMe/ContactForm.jsx
@@ -1,19 +1,75 @@
+/* eslint-disable no-unused-vars */
+
 import './ContactForm.css';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactForm = () => {
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const firstName = e.target.firstName.value;
+        const lastName = e.target.lastName.value;
+        const email = e.target.email.value;
+        const message = e.target.message.value;
+
+        emailjs.send('service_wghlsus', 'template_ewdxh2i', {
+            firstName,
+            lastName,
+            email,
+            message,
+        }, 'YL6_OLSzXe5GETMNt')
+        .then((response) => {
+            toast.success('Your email has been sent successfully!');
+            e.target.reset(); // Clear form fields
+        })
+        .catch((error) => {
+            toast.error('There was an error sending your email.');
+        });
+    };
+
     return (
         <div className='contact-form-content'>
-            <form>
+            <form onSubmit={sendEmail}>
                 <div className='name-container'>
-                    <input type="text" name='firstName' placeholder='First Name' />
-                    <input type="text" name='lastName' placeholder='Last Name' />
+                    <input type="text" name='firstName' placeholder='First Name' required />
+                    <input type="text" name='lastName' placeholder='Last Name' required />
                 </div>
-                <input type="email" name='email' placeholder='Email'/>
-                <textarea type="text" name="message" placeholder='Message'  rows={3}></textarea>
-                <button>SEND</button>
+                <input type="email" name='email' placeholder='Email' required/>
+                <textarea type="text" name="message" placeholder='Message' rows={3}></textarea>
+                <button type="submit">SEND</button>
             </form>
+            <ToastContainer />
         </div>
     );
 };
 
 export default ContactForm;
+
+
+
+
+
+
+// MAIN CODE BELOW
+
+// import './ContactForm.css';
+
+// const ContactForm = () => {
+//     return (
+//         <div className='contact-form-content'>
+//             <form>
+//                 <div className='name-container'>
+//                     <input type="text" name='firstName' placeholder='First Name' required />
+//                     <input type="text" name='lastName' placeholder='Last Name' required />
+//                 </div>
+//                 <input type="email" name='email' placeholder='Email' required/>
+//                 <textarea type="text" name="message" placeholder='Message'  rows={3}></textarea>
+//                 <button>SEND</button>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default ContactForm;
